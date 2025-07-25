8b48d704ce176de04285184575a14970980d91bb
Sasha Koss <koss@nocorp.me>
2025-07-25 22:37:45 +0000

    Add types needed for Tysts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
 export namespace $ {
+  //#region Extreme
+
+  export namespace Is {
+    export type Extreme<Type> = Or<
+      Is.Any<Type>,
+      Is.Unknown<Type>,
+      Is.Never<Type>
+    >;
+  }
+
+  //#region Top
+
   export type Top = any | unknown;
+
+  //#endregion
+
+  //#region Any
+
+  export namespace Is {
+    export type Any<Type> = 0 extends 1 & Type ? true : false;
+  }
+
+  //#endregion
+
+  //#region Unknown
+
+  export namespace Is {
+    export type Unknown<Type> = [Type] extends [unknown]
+      ? unknown extends Type
+        ? true
+        : false
+      : false;
+  }
+
+  //#endregion
+
+  //#region Never
+
+  export namespace Is {
+    export type Never<Type> = [Type] extends [never] ? true : false;
+  }
+
+  //#endregion
+
+  //#endregion
 
   export type Everything = {} | Nullish;
 
@@ -12,4 +56,59 @@ export namespace $ {
   export type Primitive = Value | Nullish;
 
   export type ObjectLike = object | {};
+
+  //#region Conditions
+
+  export type Not<Type> = Type extends true ? false : true;
+
+  export type And<
+    Type1,
+    Type2,
+    Type3 = Type2,
+    Type4 = Type3,
+    Type5 = Type4,
+    Type6 = Type5,
+    Type7 = Type6,
+    Type8 = Type7,
+    Type9 = Type8,
+    Type10 = Type9
+  > = true extends Type1 &
+    Type2 &
+    Type3 &
+    Type4 &
+    Type5 &
+    Type6 &
+    Type7 &
+    Type8 &
+    Type9 &
+    Type10
+    ? true
+    : false;
+
+  export type Or<
+    Type1,
+    Type2,
+    Type3 = Type2,
+    Type4 = Type3,
+    Type5 = Type4,
+    Type6 = Type5,
+    Type7 = Type6,
+    Type8 = Type7,
+    Type9 = Type8,
+    Type10 = Type9
+  > = true extends
+    | Type1
+    | Type2
+    | Type3
+    | Type4
+    | Type5
+    | Type6
+    | Type7
+    | Type8
+    | Type9
+    | Type10
+    ? true
+    : false;
+
+  //#endregion
 }
